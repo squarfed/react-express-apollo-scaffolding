22694e3320d8acec21b806b33d8ab37b5d6ead6c
Federico Squartini <federico.squartini@gmail.com>
2017-07-29 19:22:14 +0200

    Add basic apollo server functions

--- server/main.js
+++ server/main.js
@@ -2,7 +2,51 @@ const path = require('path')
 const express = require('express')
 const webpack = require('webpack')
 const webpackDevMiddleware = require('webpack-dev-middleware')
+const bodyParser = require('body-parser')
 const config = require('../webpack.config.js')
+const graphqlExpress = require('graphql-server-express').graphqlExpress
+const graphiqlExpress = require('graphql-server-express').graphiqlExpress
+const makeExecutableSchema = require('graphql-tools').makeExecutableSchema
+
+const typeDefs = [`
+  type Post {
+    name: String
+    title: String
+    content: String
+  }
+  type Query {
+    posts(keyword: String): [Post]
+  }
+  type Mutation {
+    addPost(name: String, title: String, content: String): Post
+  }
+  schema {
+    query: Query
+    mutation: Mutation
+  }
+  `]
+
+const postsData = [
+  { name: 'John', title: 'Hello', content: 'pen pineapple' },
+  { name: 'Mike', title: 'Hi', content: 'apple pen' }
+]
+
+const resolvers = {
+  Query: {
+    posts (root, args) {
+      return postsData.filter(post =>
+        !args.keyword || post.content.indexOf(args.keyword) >= 0)
+    },
+  },
+  Mutation: {
+    addPost (root, doc) {
+      postsData.push(doc)
+      return doc
+    },
+  }
+}
+
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 const app = express()
 const DIST_DIR = path.join(__dirname, '../dist')
@@ -24,6 +68,9 @@ if (!isProduction) {
     res.sendFile(path.join(DIST_DIR + '/index.js'))
   })
 }
+app.use('/graphql', bodyParser.json(), graphqlExpress({schema}))
+app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}))
+
 /*
  *
  *
