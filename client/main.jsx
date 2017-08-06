385eb6aa3b5d62e1c8140e23520625c43ef22800
Federico Squartini <federico.squartini@gmail.com>
2017-08-07 01:27:34 +0200

    Plug redux

--- client/main.jsx
+++ client/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 import {ApolloClient, createNetworkInterface, ApolloProvider} from 'react-apollo'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 
 import 'styles'
 import Home from 'Home'
@@ -10,9 +10,22 @@ const client = new ApolloClient({
   networkInterface: createNetworkInterface({uri: 'http://localhost:3000/graphql'})
 })
 
+const store = createStore(
+  combineReducers({
+    apollo: client.reducer()
+  }),
+  {}, // initial state
+  compose(
+    applyMiddleware(client.middleware()),
+    // If you are using the devToolsExtension, you can add it here also
+    (typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined')
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : f => f)
+)
+
 const render = () => {
   ReactDOM.render(
-    <ApolloProvider client={client}>
+    <ApolloProvider store={store} client={client}>
       <Home />
     </ApolloProvider>
     , document.getElementById('app'))
